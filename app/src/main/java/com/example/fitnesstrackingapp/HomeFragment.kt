559de0e8e30981e304dd61d8e28ce64aabe85e6d
Nyan Linn Htet <shosuke48@gmail.com>
2026-03-06 09:31:11 +0630

    Main Navigation Done

--- app/src/main/java/com/example/fitnesstrackingapp/HomeFragment.kt
+++ app/src/main/java/com/example/fitnesstrackingapp/HomeFragment.kt
@@ -5,13 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.fitnesstrackingapp.databinding.FragmentHomeBinding
+
 class HomeFragment : Fragment() {
+    private lateinit var binding: FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(layoutInflater, container, false)
+        binding.btnStart.setOnClickListener {
+            val action = HomeFragmentDirections.actionHomeFragmentToRegisterFragment()
+            findNavController().navigate(action)
+        }
+        binding.txtLogin.setOnClickListener {
+            val action = HomeFragmentDirections.actionHomeFragmentToLoginFragment()
+            findNavController().navigate(action)
+        }
+
+        return binding.root
     }
 }
--- app/src/main/java/com/example/fitnesstrackingapp/MainActivity.kt
+++ app/src/main/java/com/example/fitnesstrackingapp/MainActivity.kt
@@ -2,12 +2,26 @@ package com.example.fitnesstrackingapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.fitnesstrackingapp.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.mainFragmentView) as NavHostFragment
+        navController = navHostFragment.navController
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
