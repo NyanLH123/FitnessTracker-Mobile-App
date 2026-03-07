2010452334211d1bff36b0714ffae4b489c78091
Nyan Linn Htet <shosuke48@gmail.com>
2026-03-07 22:43:51 +0630

    Update

--- app/src/main/java/com/example/fitnesstrackingapp/RegisterFragment.kt
+++ app/src/main/java/com/example/fitnesstrackingapp/RegisterFragment.kt
@@ -5,15 +5,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.fitnesstrackingapp.databinding.FragmentRegisterBinding
 
 class RegisterFragment : Fragment() {
-
+    private lateinit var binding: FragmentRegisterBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false)
+        binding = FragmentRegisterBinding.inflate(layoutInflater, container, false)
+        binding.btnRegister.setOnClickListener {
+            val firstName = binding.editFirstName.text.toString()
+            val lastName = binding.editLastName.text.toString()
+            val username = binding.editUsername.text.toString()
+            val email = binding.editEmail.text.toString()
+            val password = binding.editPassword.text.toString()
+            val confirmPass = binding.editConfirmPass.text.toString()
+
+
+        }
+
+        return binding.root
+    }
+
+    private fun validateInput(firstName: String, lastName: String, username: String, email: String, password: String, confirmPass: String): Boolean {
+        var isValid = true
+
+        if (email.isEmpty()) {
+            binding.
+        }
     }
 
 }
